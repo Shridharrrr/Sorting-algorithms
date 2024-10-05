c5089a251e985f2ca51f1444b2e6c46eada01ad3
Shridhar Mandrekar <geekyshridhar04@gmail.com>
2024-10-06 00:54:11 +0530

    Update bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,29 +1,37 @@
 #include<stdio.h>
-int main(){
-    int n;
-    printf("Enter size of array : ");
-    scanf("%d",&n);
-    
-    int arr[n];
-    
-    for(int i=0;i<n;i++){
-    printf("Enter %d num : ",i+1);
-    scanf("%d",&arr[i]);
+#include<stdbool.h>
+void inputarray(int array[],int size){
+    for(int i = 0; i < size; i++){
+    printf("Enter %d element of array : ",i+1);
+    scanf("%d",&array[i]);
     }
-    
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-1-i;j++){
+}
+void outputarray(int array[],int size){
+    for(int i = 0; i < size; i++){
+        printf("%d ",array[i]);
+    }
+}
+int main(){
+    int size;
+    printf("Enter size of array : ");
+    scanf("%d",&size);
+
+    int arr[size];
+    inputarray(arr,size);
+    bool flag = false;
+    for(int i=0;i<size;i++){
+        for(int j=0;j<size-1-i;j++){
             if(arr[j]>arr[j+1]){
             int tempt = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = tempt;
+            flag = true;
             }
-        }  
+        } 
+        if(flag==false)break; 
     }   
     
-    printf("Sorted array : ");
-    for(int i=0;i<n;i++){
-    printf("%d ", arr[i]);
-    }
+    printf("Array after sorting is : ");
+    outputarray(arr,size);
 return 0;
 }    
